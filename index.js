97167e6b54872b481591223bd4a22ef1ab9fa4b0
gleiMoura <gle.bar.mou@gmail.com>
2024-05-07 21:04:24 -0300

    ✨ feat: create a function to input element put the name in a child

---
+++ index.js
@@ -0,0 +1,8 @@
+function darNome() {
+    const inputValue = document.getElementById("input").value;
+    //input = document.getElementById("nome do id que você colocou no HTML").value
+
+    //precisa de um evento para que o inputValue apareça na tela do console.
+
+    console.log(inputValue) //O elemento HTML EXISTE e estamos pegando ele.
+};
