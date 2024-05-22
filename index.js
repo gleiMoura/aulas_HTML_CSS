8752b86b5e832ac5a14bc486bea178292a88ead8
gleiMoura <gle.bar.mou@gmail.com>
2024-05-22 19:34:50 -0300

    ♻️ change functions

--- index.js
+++ index.js
@@ -1,17 +1,33 @@
-function darNome() {
+function criarCrianca() {
     const inputValue = document.getElementById("input").value;
     const childBigBox = document.getElementById("child_011");
 
-    //template string
-    childBigBox.innerHTML = `
-        <div class="child_box" id="child_021">
+    /* childBigBox.innerHTML = ''; */ //Anula o que tem dentro do elemento dom
+
+    for (let i = 0; i < 3; i++) {
+        let numeroDoId = 2 + i;
+        let id = 'child_02' + numeroDoId;
+
+        childBigBox.innerHTML += `
+        <div class="child_box" id="child_02${numeroDoId}">
             <p>${inputValue}</p>
+            <button onclick="desaparecer('${id}')">Desapareça!</button>
         </div>
-    `;
-    
+        `;
+    };
+
+    /*  let constante = 10;
+        constante = constante + 1;
+        constante++; */
+    //i++ --> soma um
+
     //sem template string
     /* const nome = "eu tenho" + anos + "anos"
     <p>nome</p> */
-
-    console.log(inputValue)
 };
+
+function desaparecer(id) {
+    const elemento = document.getElementById(id);
+    elemento.style.display = 'none';
+}
+
